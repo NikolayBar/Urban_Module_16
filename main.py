d81883f7374d2b7dc23edfc3b9be258a56fbef3e
NikolayBar <nikolbar@yandex.ru>
2024-12-18 22:50:29 +0300

    HW 16_01

--- main.py
+++ main.py
@@ -1,42 +1,43 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+#
+#
+# class Item(BaseModel):
+#     name: str
+#     price: float
+#
+#
+# app = FastAPI()
+#
+#
+# @app.get("/")
+# async def root() -> dict:
+#     return {"message": "Hello FastAPI"}
+#
+#
+# # @app.get("/main")
+# # async def welcome() -> dict:
+# #     return {"message": "Main page"}
+#
+#
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: int):
+#     return {'item_id': item_id}
+#
+#
+# @app.post("/items/")
+# async def create_item(item: Item):
+#     return {"name": item.name, "price": item.price}
+#
+#
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item):
+#     return {"item_id": item_id,
+#             "name": item.name,
+#             "price": item.price}
+#
+#
+# @app.delete("/items/{item_id}")
+# async def delete_item(item_id: int):
+#     return {"message": "Item deleted", "item_id": item_id}
 
-
-class Item(BaseModel):
-    name: str
-    price: float
-
-
-app = FastAPI()
-
-
-@app.get("/")
-async def root() -> dict:
-    return {"message": "Hello FastAPI"}
-
-
-# @app.get("/main")
-# async def welcome() -> dict:
-#     return {"message": "Main page"}
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {'item_id': item_id}
-
-
-@app.post("/items/")
-async def create_item(item: Item):
-    return {"name": item.name, "price": item.price}
-
-
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-    return {"item_id": item_id,
-            "name": item.name,
-            "price": item.price}
-
-
-@app.delete("/items/{item_id}")
-async def delete_item(item_id: int):
-    return {"message": "Item deleted", "item_id": item_id}

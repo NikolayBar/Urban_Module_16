54b37b18ca35b6c1127ba9106bc9ce274d583ed4
NikolayBar <nikolbar@yandex.ru>
2024-12-20 00:09:31 +0300

    HW 16_01 dict -> str

--- module_16_01.py
+++ module_16_01.py
@@ -4,20 +4,20 @@ app = FastAPI()
 
 
 @app.get("/")
-async def main_page() -> dict:
-    return {"message": "Главная страница"}
+async def main_page() -> str:
+    return "Главная страница"
 
 
 @app.get("/user/admin")
-async def admin_page() -> dict:
-    return {"message": "Вы вошли как администратор"}
+async def admin_page() -> str:
+    return "Вы вошли как администратор"
 
 
 @app.get("/user/{user_id}")
-async def user_page(user_id: int) -> dict:
-    return {"message": f"Вы вошли как пользователь № {user_id}"}
+async def user_page(user_id: int) -> str:
+    return f"Вы вошли как пользователь № {user_id}"
 
 
 @app.get("/user")
-async def user_info(username: str = 'Ilya', age: int = 24) -> dict:
-    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
+async def user_info(username: str = 'Ilya', age: int = 24) -> str:
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
